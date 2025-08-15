8a9e717a5d2928d48ab40c073d40dc34f0cba977
Janis Löns <janis.loens@outlook.de>
2025-08-15 21:40:59 +0200

    Connected database, created test route

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 from flask import Flask, request, jsonify
+from data_manager import DataManager
+from models import db, Movie
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+
+db.init_app(app)
+
+data_manager = DataManager()
+
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True)
